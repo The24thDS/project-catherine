e8b8081f31d63ddbaf1c70807826004c3f156148
The24thDS <davidsima@windowslive.com>
2020-03-30 17:54:33 +0300

    Redux initialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+
+import logo from "./logo.svg";
+import "./App.sass";
+
+class App extends React.Component {
+  componentDidMount() {
+    // just an example
+    const { setCurrentUser } = this.props;
+    // this user will be available in this.props.currentUser
+    setCurrentUser({ name: "Catherine", id: 5 });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>Project Catherine</p>
+            </header>
+          </Route>
+          {/* this route will render if all of the above routes don't */}
+          <Route>404 - Not Found</Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+// this function will select from the store and map the results to props
+// propName: selectorName
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
+
+// this function will map boundActionCreators to props
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store";
+
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
