1e22a3f2ad98d0a766d37cef725c6c046359f157
mihailpostica <55237403+mihailpostica@users.noreply.github.com>
2020-04-08 22:41:10 +0300

    modified login payload to ask for email insted of user. Changed the activation enpoint to work with token only

--- server/src/main/java/com/example/demo/controllers/AuthController.java
+++ server/src/main/java/com/example/demo/controllers/AuthController.java
@@ -10,9 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.MailParseException;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -22,8 +19,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import com.sendgrid.*;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 import javax.validation.Valid;
 
 import java.io.IOException;
@@ -49,15 +44,14 @@ public class AuthController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-    @Autowired
-    private JavaMailSender javaMailSender;
+
 
     @PostMapping("/register")
     @ResponseBody
-    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest registerRequest, BindingResult bindingResult) throws IOException{
-        if(bindingResult.hasErrors()){
-            ArrayList<String>stringErrors=new ArrayList<>();
-            List<FieldError> errors= bindingResult.getFieldErrors();
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest registerRequest, BindingResult bindingResult) throws IOException {
+        if (bindingResult.hasErrors()) {
+            ArrayList<String> stringErrors = new ArrayList<>();
+            List<FieldError> errors = bindingResult.getFieldErrors();
             for (FieldError fieldError : errors) {
                 String error = fieldError.getField();
                 if (!stringErrors.contains(error)) {
@@ -67,11 +61,11 @@ public class AuthController {
 
             throw new UserRegistrationInvalidFieldsException(stringErrors);
         }
-        if(userRepository.existsByEmail(registerRequest.getEmail())){
-            ApiResponse userRegistrationResponse=new ApiResponse("email Already taken",false);
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            ApiResponse userRegistrationResponse = new ApiResponse("email Already taken", false);
             return new ResponseEntity<>(userRegistrationResponse, HttpStatus.BAD_REQUEST);
         }
-        User user=new User();
+        User user = new User();
         user.setEmail(registerRequest.getEmail());
         user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
         user.setEnabled(false);
@@ -79,10 +73,10 @@ public class AuthController {
         user.setFirst_name(registerRequest.getFirst_name());
         user.setLast_name(registerRequest.getLast_name());
         user.setBirth_date(registerRequest.getBirth_date());
-        VerificationToken verificationToken=new VerificationToken();
-        String token=UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        String token = UUID.randomUUID().toString();
         verificationToken.setToken(token);
-        user.setVerificationToken( verificationToken);
+        user.setVerificationToken(verificationToken);
         userRepository.save(user);
 
         SendGrid sg = new SendGrid(sendGridApiKey);
@@ -90,14 +84,14 @@ public class AuthController {
             Request request = new Request();
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
-            request.setBody("{"+
-            "\"from\": {\"email\": \"hello@project-catherine.wtf\"},"+
-            "\"personalizations\":[{"+
-                "\"to\": [{\"email\": \""+registerRequest.getEmail()+"\"}],"
-                +"\"dynamic_template_data\": {"+
-                    "\"activation_token\": \""+verificationToken.getToken()+"\""+
-                "}}],"+
-            "\"template_id\": \"d-594fb91f070e41fdab678d9a50842324\"}");
+            request.setBody("{" +
+                    "\"from\": {\"email\": \"hello@project-catherine.wtf\"}," +
+                    "\"personalizations\":[{" +
+                    "\"to\": [{\"email\": \"" + registerRequest.getEmail() + "\"}],"
+                    + "\"dynamic_template_data\": {" +
+                    "\"activation_token\": \"" + verificationToken.getToken() + "\"" +
+                    "}}]," +
+                    "\"template_id\": \"d-594fb91f070e41fdab678d9a50842324\"}");
             Response response = sg.api(request);
             System.out.println(response.getStatusCode());
             System.out.println(response.getBody());
@@ -106,22 +100,7 @@ public class AuthController {
             throw ex;
         }
 
-        // MimeMessage message =javaMailSender.createMimeMessage();
-        // try {
-        //     MimeMessageHelper helper = new MimeMessageHelper(message, false, "utf-8");
-        //     String htmlMsg = "<body style='border:2px solid black'>"
-        //             +"<h1>Your onetime password for registration is : </h1><h2>"+verificationToken.getToken()+"</h2>"
-        //             + "Please use this OTP to complete your new user registration."+
-        //             "OTP is confidential, do not share this  with anyone." +
-        //             "</body>";
-        //     message.setContent(htmlMsg, "text/html");
-        //     helper.setTo(registerRequest.getEmail());
-        //     helper.setSubject("Account Registration");
-        //     javaMailSender.send(message);
-        // } catch (MessagingException e) {
-        //     throw new MailParseException(e);
-        // }
-        return new ResponseEntity<>(new ApiResponse("account created successfully",true), HttpStatus.CREATED);
+        return new ResponseEntity<>(new ApiResponse("account created successfully", true), HttpStatus.CREATED);
 
     }
 
@@ -130,48 +109,37 @@ public class AuthController {
 
         try {
             authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginRequest.getUser()
+                    new UsernamePasswordAuthenticationToken(loginRequest.getEmail()
                             , loginRequest.getPassword())
 
             );
 
-        }
-        catch (BadCredentialsException e) {
+        } catch (BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new BadCredentialsResponse("bad credentials"));
         }
-        final UserDetails userDetails = myUserDetailsService.loadUserByUsername(loginRequest.getUser());
+        final UserDetails userDetails = myUserDetailsService.loadUserByUsername(loginRequest.getEmail());
         final String jwt = jwtTokenUtil.generateToken(userDetails);
-        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(jwt,true));
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(jwt, true));
     }
 
 
     @PostMapping("/verifyToken")
-    public ResponseEntity<?> verifyToken(@RequestBody AvailabilityRequest availabilityRequest){
-        String jwt=availabilityRequest.getInput();
-        String username=jwtTokenUtil.extractUsername(jwt);
+    public ResponseEntity<?> verifyToken(@RequestBody AvailabilityRequest availabilityRequest) {
+        String jwt = availabilityRequest.getInput();
+        String username = jwtTokenUtil.extractUsername(jwt);
         UserDetails userDetails = this.myUserDetailsService.loadUserByUsername(username);
-        if(jwtTokenUtil.validateToken(jwt,userDetails)){
-            return new ResponseEntity<>(new ApiResponse("token is valid",true), HttpStatus.OK);
-        }
-        else return new ResponseEntity<>(new ApiResponse("token is invalid",false), HttpStatus.BAD_REQUEST);
+        if (jwtTokenUtil.validateToken(jwt, userDetails)) {
+            return new ResponseEntity<>(new ApiResponse("token is valid", true), HttpStatus.OK);
+        } else return new ResponseEntity<>(new ApiResponse("token is invalid", false), HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping("/enableAccount")
-    public ResponseEntity<?>enableAccount(@RequestBody EnableAccountRequest enableAccountRequest) {
-        Optional<User> user = userRepository.findByEmail(enableAccountRequest.getUser());
+    public ResponseEntity<?> enableAccount(@RequestBody EnableAccountRequest enableAccountRequest) {
+        Optional<User> user = userRepository.findByVerificationToken(enableAccountRequest.getToken());
         if (user.isPresent()) {
-            Optional<VerificationToken> verificationToken = userRepository.findVerificationToken(user.get().getEmail());
-            if(verificationToken.isPresent()) {
-                if (verificationToken.get().getToken().equals(enableAccountRequest.getToken())) {
-                    user.get().setEnabled(true);
-                    userRepository.save(user.get());
-                    return new ResponseEntity<>(new ApiResponse("Account activated successfully", true), HttpStatus.OK);
-                } else return new ResponseEntity<>(new ApiResponse("Token is invalid", false), HttpStatus.OK);
-            }else return new ResponseEntity<>(new ApiResponse("A token for this account has not been created yet", false), HttpStatus.OK);
-        }else return new ResponseEntity<>(new ApiResponse("Account with such username or email doesn't exist", false), HttpStatus.OK);
-
+            user.get().setEnabled(true);
+            userRepository.save(user.get());
+            return new ResponseEntity<>(new ApiResponse("Account activated successfully!", true), HttpStatus.OK);
+        } else return new ResponseEntity<>(new ApiResponse("Error! Could not activate your account", true), HttpStatus.OK);
     }
-
-
-
 }
