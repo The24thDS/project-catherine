41add0b01287eab51fc1e8b94850eecd4206766e
The24thDS <davidsima@windowslive.com>
2020-05-19 18:21:50 +0300

    Fix for double import

--- src/components/NavBar/DropDownMenu/MenuItems/FriendRequestItem/FriendRequestItem.jsx
+++ src/components/NavBar/DropDownMenu/MenuItems/FriendRequestItem/FriendRequestItem.jsx
@@ -8,7 +8,6 @@ import { setFriendsInfo } from "../../../../../redux/friends/friends.actions";
 import { getUserFriends } from "../../../../../utils/user";
 
 import styles from "./FriendRequestItem.module.sass";
-import ServerRequest from "../../../../../utils/ServerRequest";
 
 class FriendRequestItem extends Component {
   state = {
