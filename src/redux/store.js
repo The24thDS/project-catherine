2c1b22d9668a18e3eabcabc32030f9fd58bffd71
The24thDS <davidsima@windowslive.com>
2020-05-19 17:56:29 +0300

    Preparing for production

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,17 +5,21 @@ import LogRocket from "logrocket";
 import rootReducer from "./rootReducer";
 
 const middlewares = [];
+middlewares.push(LogRocket.reduxMiddleware());
 
-if (process.env.NODE_ENV === "development") {
-  middlewares.push(LogRocket.reduxMiddleware());
+let store;
+
+if (process.env.REACT_APP_ENV === "development") {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(...middlewares))
+  );
 }
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(...middlewares))
-);
+if (process.env.REACT_APP_ENV === "production") {
+  store = createStore(rootReducer, applyMiddleware(...middlewares));
+}
 
 const persistor = persistStore(store);
 
